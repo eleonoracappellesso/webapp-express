933b8f6c06823c89b67b67e65b03fc3d2eeae1d0
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-01-23 15:35:42 +0100

    created index route

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,7 +1,18 @@
 const connection = require("../connection.js");
 
 function index(req, res) {
+    const sql = 'SELECT * FROM `movies`'
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        console.log(results);
+        let data = results;
+        const response = {
+            totalCount: results.length,
+            data,
+        };
+        res.json(response);
 
+    });
 }
 
 function show(req, res) {
